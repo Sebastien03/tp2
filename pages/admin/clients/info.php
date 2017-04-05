2a03fe8d08aa2ca0f5cfe2beb1479876c2cf6bc1
Sebastien Dutripon <seb032@live.fr>
2017-04-05 10:39:02 +0200

    correction affichage

--- pages/admin/clients/info.php
+++ pages/admin/clients/info.php
@@ -12,34 +12,39 @@
 <div class="row">
     <div class="col-md-12">
         <h2>Informations détaillés sur le client</h2>
-        <hr>
-
-        <p><strong>Prénom:</strong> <?= $client->identite ?></p>
-        <p><strong>Âge:</strong> <?= $client->age ?></p>
-        <p><strong>Date de naissance:</strong> <?= $client->date_de_naissance ?></p>
-        <p><strong>Adresse:</strong> <?= $client->adresse ?></p>
-        <p><strong>Code postal:</strong> <?= $client->code_postal ?></p>
-        <p><strong>Téléphone:</strong> <?= $client->numero_de_telephone ?></p>
-        <p><strong>Situation familiale:</strong> <?= $client->statut ?></p>
-        <p>
-            <strong>Organisation:</strong>
+        <table class="table table-bordered">
+            <thead>
+                <th> Identite </th>
+                <th> Age </th>
+                <th> Date de naissance </th>
+                <th> Adresse</th>
+                <th> Telephone </th>
+                <th> Statut </th>
+                <th> Organisme </th>
+                <th> Montant </th>
+            </thead>
+            <tbody>
+                <td>  <?= $client->identite ?> </td>
+                <td>  <?= $client->age ?> </td>
+                <td> <?= $client->date_de_naissance ?> </td>
+                <td> <?= $client->adresse_complete ?> </td>
+                <td> <?= $client->numero_de_telephone ?> </td>
+                <td> <?= $client->statut ?> </td>
+                <td>  <?php
+                foreach($credits as $credit){
+                    if($credit->clients_id == $client->id){
+                        echo ' '.$credit->organisme.', ';
+                    }
+                }
+            ?> </td>
+            <td> 
             <?php
                 foreach($credits as $credit){
                     if($credit->clients_id == $client->id){
-                        echo $credit->organisme;
+                        echo ' '.$credit->montant.'€'.', ';
                     }
                 }
             ?>
-        </p>
-        <p>
-            <strong>Montant:</strong>
-            <?php
-                foreach($credits as $credit){
-                    if($credit->clients_id == $client->id){
-                        echo $credit->montant.' €';
-                    }
-                }
-            ?>
-        </p>
+        </td>
     </div>
 </div>
